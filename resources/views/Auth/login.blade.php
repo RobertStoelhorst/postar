21ede4a5ef77f75194ee3330d4cae41b369662a3
RobertStoelhorst <robbiestoelhorst@hotmail.com>
2021-03-23 06:13:23 +1100

    user login validation

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -3,26 +3,29 @@
 @section('content')
 <div class="flex justify-center">
    <div class="w-5/12 bg-white p-6 rounded-lg">
-       <form action="{{ route('register') }}" method="post">
+
+    @if (session()->has('status'))
+        {{ session('status') }}
+    @endif
+
+       <form action="{{ route('login') }}" method="post">
         @csrf
         <div class="mb-4">
             <label for="email" class="sr-only">Email</label>
             <input type="text" name="email" id="name" placeholder="Your email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-400 @enderror" value="{{ old('email') }}">
+           
             @error('email')
             <div class="text-red-500 mt-2 text-sm">
                 {{ $message }}
             </div>
             @enderror
         </div>
+
         <div class="mb-4">
             <label for="password" class="sr-only">Password</label>
-            <input type="password" name="password" id="password" placeholder="Choose a password" class="bg-gray-100 border-2 w-half p-4 rounded-lg @error('password') border-red-400 @enderror" value="">
-            @error('password')
-            <div class="text-red-500 mt-2 text-sm">
-                {{ $message }}
-            </div>
-            @enderror
+            <input type="password" name="password" id="password" placeholder="Choose a password" class="bg-gray-100 border-2 w-half p-4 rounded-lg" value="">
         </div>
+
         <div>
             <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Login</button>
         </div>
