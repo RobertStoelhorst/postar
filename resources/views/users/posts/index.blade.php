2c61a8217735491825c37a43b834bb7403877714
RobertStoelhorst <robbiestoelhorst@hotmail.com>
2021-03-31 09:56:05 +1100

    some markup to show and pluralize user posts count()

--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -2,16 +2,24 @@
 
 @section('content')
 <div class="flex justify-center">
-   <div class="w-8/12 bg-white p-6 rounded-lg">
-        @if($posts->count())
-            @foreach ($posts as $post) {{-- post models --}}
-                <x-post :post="$post" /> {{-- pass down :post="$post" like a prop into the component which is here ( /Users/robertstoelhorst/projects/PHP/posty-web-app/resources/views/components/post.blade.php )--}}
-            @endforeach
-            
-            {{ $posts->links() }} {{-- this is how we set-up and all we have to do for the pagination links controller below the posts, also set the paginate() and amount in the PostController --}}
-        @else
-            <p>{{ $user->name }} does not have any posts</p>
-        @endif
+   <div class="w-8/12">
+
+<div class="p-6">
+<h1 class="text-2xl font-medium mb-1">{{ $user->name }}</h1>
+<p>Posted {{ $posts->count() }} {{ Str::plural('post', $posts->count()) }}</p>
+</div>
+
+        <div class="bg-white p-6 rounded-lg">
+            @if($posts->count())
+                @foreach ($posts as $post) {{-- post models --}}
+                    <x-post :post="$post" /> {{-- pass down :post="$post" like a prop into the component which is here ( /Users/robertstoelhorst/projects/PHP/posty-web-app/resources/views/components/post.blade.php )--}}
+                @endforeach
+                
+                {{ $posts->links() }} {{-- this is how we set-up and all we have to do for the pagination links controller below the posts, also set the paginate() and amount in the PostController --}}
+            @else
+                <p>{{ $user->name }} does not have any posts</p>
+            @endif
+        </div>    
    </div>
 </div>
 
